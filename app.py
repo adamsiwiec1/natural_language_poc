f487011a629c026ed492a03daff3f2fb7b353100
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-07 18:44:01 -0500

    sentiment pdf feature added

--- app.py
+++ app.py
@@ -5,22 +5,17 @@ import json
 from flask import Flask, render_template, request
 from flask_restful import Resource, Api, reqparse
 from PyPDF2 import PdfFileReader
-from google.cloud import language_v1
-import google
 
 app=Flask(__name__)
 api=Api(app)
-app.config['UPLOAD_FOLDER'] = r'Z:\.GCP\natural_language_poc\pdfs'
-
-nlp_client = language_v1.LanguageServiceClient()
+app.config['UPLOAD_FOLDER']=r'Z:\.GCP\natural_language_poc\pdfs'
 
 
-# you should create a class for each endpoint - with get/post/put/etc.. methods
 class AnalyzeEntities(Resource):
     def post(self) -> list:
-        parser = reqparse.RequestParser()
+        parser=reqparse.RequestParser()
         parser.add_argument('text', required=True)  # add args
-        args = parser.parse_args()
+        args=parser.parse_args()
 
         body={
             "document": {
@@ -33,7 +28,7 @@ class AnalyzeEntities(Resource):
         response=requests.post('https://language.googleapis.com/v1beta2/documents:analyzeEntities'
                                f'?key={api_key}',
                                data=json.dumps(body))
-        d = json.loads(response.text)
+        d=json.loads(response.text)
         return [d.get('entities')[i].get('name') for i, x in enumerate(d.get('entities'))]
 
 
@@ -41,11 +36,11 @@ class PDFAnalyzeEntities(Resource):
     def post(self) -> list:
         if request.method == 'POST':
             f=request.files['file']
-            path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+            path=os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
             f.save(path)
-            text = extract_pdf(path)
+            text=extract_pdf(path)
 
-            body = {
+            body={
                 "document": {
                     "type": "PLAIN_TEXT",
                     "language": "EN",
@@ -56,16 +51,16 @@ class PDFAnalyzeEntities(Resource):
             response=requests.post('https://language.googleapis.com/v1beta2/documents:analyzeEntities'
                                    f'?key={api_key}',
                                    data=json.dumps(body))
-            d = json.loads(response.text)
+            d=json.loads(response.text)
             print(d)
             return [d.get('entities')[i].get('name') for i, x in enumerate(d.get('entities'))]
 
 
 class AnalyzeEntitySentiment(Resource):
     def post(self) -> list:
-        parser = reqparse.RequestParser()
+        parser=reqparse.RequestParser()
         parser.add_argument('text', required=True)  # add args
-        args = parser.parse_args()
+        args=parser.parse_args()
 
         body={
             "document": {
@@ -75,56 +70,69 @@ class AnalyzeEntitySentiment(Resource):
             },
             "encodingType": "UTF8"
         }
-        response=requests.post('https://language.googleapis.com/v1beta2/documents:analyzeEntities'
+        response=requests.post('https://language.googleapis.com/v1beta2/documents:analyzeEntitySentiment'
                                f'?key={api_key}',
                                data=json.dumps(body))
-        d = json.loads(response.text)
+        d=json.loads(response.text)
         print(d)
         return [d.get('entities')[i].get('name') for i, x in enumerate(d.get('entities'))]
 
 
 class PDFAnalyzeEntitySentiment(Resource):
     def post(self) -> list:
-        parser = reqparse.RequestParser()
-        parser.add_argument('text', required=True)  # add args
-        args = parser.parse_args()
+        f=request.files['file']
+        path=os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+        f.save(path)
+        text=extract_pdf(path)
 
         body={
             "document": {
                 "type": "PLAIN_TEXT",
                 "language": "EN",
-                "content": args.get('text')
+                "content": text
             },
             "encodingType": "UTF8"
         }
-        response=requests.post('https://language.googleapis.com/v1beta2/documents:analyzeEntities'
+        response=requests.post('https://language.googleapis.com/v1beta2/documents:analyzeEntitySentiment'
                                f'?key={api_key}',
                                data=json.dumps(body))
-        d = json.loads(response.text)
-        return [d.get('entities')[i].get('name') for i, x in enumerate(d.get('entities'))]
+        d=json.loads(response.text)
+        print(d)
 
-
-@app.route('/upload_pdf')
-def upload_pdf():
-    return render_template('upload.html')
+        return [(d.get('entities')[i].get('name'),
+                 [(mention.get('text').get('content'),
+                   mention.get('type'),
+                   mention.get('sentiment').get('magnitude'),
+                   mention.get('sentiment').get('score')) for mention in d.get('entities')[i].get('mentions')])
+                for i, x in enumerate(d.get('entities'))
+                if True in [True if mention.get('type') != 'COMMON' and
+                            mention.get('sentiment').get('magnitude') > 0 or
+                            mention.get('sentiment').get('score') > 0
+                            else False
+                            for mention in d.get('entities')[i].get('mentions')]]
 
 
 def extract_pdf(pdf_path):
     with open(pdf_path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        information = pdf.getDocumentInfo()
-        number_of_pages = pdf.getNumPages()
-        extracted_text = ''
+        pdf=PdfFileReader(f)
+        information=pdf.getDocumentInfo()
+        number_of_pages=pdf.getNumPages()
+        extracted_text=''
         for x in range(number_of_pages):
-            page_obj = pdf.getPage(x)
-            extracted_text += page_obj.extractText()
+            page_obj=pdf.getPage(x)
+            extracted_text+=page_obj.extractText()
 
-    pdf_details = f"""
+    pdf_details=f"""
     Information about {pdf_path}:\nAuthor: {information.author}
     Creator: {information.creator}\nProducer: {information.producer}
     Subject: {information.subject}\nTitle: {information.title}\nNumber of pages: {number_of_pages}
     """
     return extracted_text
+
+
+@app.route('/upload_pdf')
+def upload_pdf():
+    return render_template('upload.html')
 
 
 api.add_resource(AnalyzeEntities, '/analyze_entities')
@@ -133,11 +141,5 @@ api.add_resource(PDFAnalyzeEntities, '/pdf/analyze_entities')
 api.add_resource(PDFAnalyzeEntitySentiment, '/pdf/analyze_entity_sentiment')
 
 if __name__ == '__main__':
-    #extract_pdf(r'Z:\.GCP\natural_language_poc\pdfs\Adam Siwiec UM form.pdf')
+    # extract_pdf(r'Z:\.GCP\natural_language_poc\pdfs\Adam Siwiec UM form.pdf')
     app.run()
-
-
-
-
-
-
