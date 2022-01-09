21086ca14551352eeefb03197017ab1a1c3c5e79
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-09 10:47:59 -0500

    sync

--- endpoints/pdf.py
+++ endpoints/pdf.py
@@ -6,12 +6,16 @@ from flask import request, Blueprint
 from helpers import Helpers, nlp_url
 
 h=Helpers()
-pdf_analyze_entities_bp=Blueprint('pdf_analyze_entities_endpoint', __name__)
-pdf_analyze_entity_sentiment_bp=Blueprint('pdf_analyze_entity_sentiment_endpoint', __name__)
+pdf_analyze_entities_bp=Blueprint('pdf_analyze_entities', __name__)
+pdf_analyze_entity_sentiment_bp=Blueprint('pdf_analyze_entity_sentiment', __name__)
+pdf_analyze_sentiment_bp=Blueprint('pdf_analyze_sentiment', __name__)
+pdf_analyze_syntax_bp=Blueprint('pdf_analyze_syntax', __name__)
+pdf_annotate_text_bp=Blueprint('pdf_annotate_text', __name__)
+pdf_classify_text_bp=Blueprint('pdf_classify_text', __name__)
 
 
 @pdf_analyze_entities_bp.route('/pdf/analyze_entities', methods=['POST'])
-def pdf_analyze_entities():
+def analyze_entities():
     if request.method == 'POST':
         f=request.files['file']
         path=os.path.join(upload_folder, f.filename)
@@ -37,7 +41,7 @@ def pdf_analyze_entities():
 
 
 @pdf_analyze_entity_sentiment_bp.route('/pdf/analyze_entity_sentiment', methods=['POST'])
-def pdf_analyze_entity_sentiment():
+def analyze_entity_sentiment():
     f=request.files['file']
     path=os.path.join(upload_folder, f.filename)
     f.save(path)
@@ -73,3 +77,99 @@ def pdf_analyze_entity_sentiment():
                                                                    for mention in d.get('entities')[i].get('mentions')
                                                                    if mention.get('sentiment').get('magnitude') > 0 or
                                                                    mention.get('sentiment').get('score') > 0]]})
+
+
+@pdf_analyze_sentiment_bp.route('/pdf/analyze_sentiment', methods=['POST'])
+def analyze_sentiment():
+    if request.method == 'POST':
+        f=request.files['file']
+        path=os.path.join(upload_folder, f.filename)
+        f.save(path)
+        pdf=h.extract_pdf(path)
+        body={"document": {
+            "type": "PLAIN_TEXT",
+            "language": "EN",
+            "content": pdf.get('text')
+        }, "encodingType": "UTF8"}
+
+        response=requests.post(f'{nlp_url}:analyzeSentiment'
+                               f'?key={api_key}',
+                               data=json.dumps(body))
+        d=json.loads(response.text)
+        print('sentiment:' + str(d))
+        h.cost_calculator(pdf.get('text'), 'content_classification')
+        return json.dumps(d)
+
+
+@pdf_analyze_syntax_bp.route('/pdf/analyze_syntax', methods=['POST'])
+def analyze_syntax():
+    if request.method == 'POST':
+        f=request.files['file']
+        path=os.path.join(upload_folder, f.filename)
+        f.save(path)
+        pdf=h.extract_pdf(path)
+        body={"document": {
+            "type": "PLAIN_TEXT",
+            "language": "EN",
+            "content": pdf.get('text')
+        }, "encodingType": "UTF8"}
+
+        response=requests.post(f'{nlp_url}:analyzeSyntax'
+                               f'?key={api_key}',
+                               data=json.dumps(body))
+        d=json.loads(response.text)
+        print('classify:' + str(d))
+        h.cost_calculator(pdf.get('text'), 'content_classification')
+        return json.dumps(d)
+
+
+@pdf_annotate_text_bp.route('/pdf/annotate_text', methods=['POST'])
+def annotate_text():
+    if request.method == 'POST':
+        f=request.files['file']
+        path=os.path.join(upload_folder, f.filename)
+        f.save(path)
+        pdf=h.extract_pdf(path)
+        body={"document": {
+            "type": "PLAIN_TEXT",
+            "language": "EN",
+            "content": pdf.get('text')
+        }, "encodingType": "UTF8",
+            'features': {
+                "extractSyntax": True,
+                "extractEntities": True,
+                "extractDocumentSentiment": True,
+                "extractEntitySentiment": True,
+                "classifyText": True
+            }
+        }
+
+        response=requests.post(f'{nlp_url}:annotateText'
+                               f'?key={api_key}',
+                               data=json.dumps(body))
+        d=json.loads(response.text)
+        print('annotate:' + str(d))
+        h.cost_calculator(pdf.get('text'), 'content_classification')
+        return json.dumps(d)
+
+
+@pdf_classify_text_bp.route('/pdf/classify_text', methods=['POST'])
+def classify_text():
+    if request.method == 'POST':
+        f=request.files['file']
+        path=os.path.join(upload_folder, f.filename)
+        f.save(path)
+        pdf=h.extract_pdf(path)
+        body={"document": {
+            "type": "PLAIN_TEXT",
+            "language": "EN",
+            "content": pdf.get('text')
+        }}
+
+        response=requests.post(f'{nlp_url}:classifyText'
+                               f'?key={api_key}',
+                               data=json.dumps(body))
+        d=json.loads(response.text)
+        print('classify:' + str(d))
+        h.cost_calculator(pdf.get('text'), 'content_classification')
+        return json.dumps(d)
