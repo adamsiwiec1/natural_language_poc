210f6fa0f2986ea5a61210920a868f32d3420482
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-12 16:20:53 -0500

    test

--- __init__.py
+++ __init__.py
@@ -10,8 +10,6 @@ from endpoints.raw import raw as raw_blueprint
 from models import User,init_db
 import os
 
-url_for.__name__
-
 app=Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY']='secret-key-goes-here'
