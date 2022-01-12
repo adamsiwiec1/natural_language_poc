7a3ec83625294940e62b504759f11e869102dcec
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-11 21:25:33 -0500

    prod client

--- __init__.py
+++ __init__.py
@@ -43,6 +43,5 @@ def page_not_found(error):
     return render_template('/shared/partials/page_not_found.html'), 404
 
 
-
 def run():
     serve(app,host='0.0.0.0', port=5000)
--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
-from  models import User
+from models import User
 
 auth=Blueprint('auth', __name__)
 
