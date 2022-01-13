131a2c4c4558a055c4b5860698c0f2581478ca20
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-13 05:15:12 -0500

    test status check

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
 
 
-api_key = 'AIzaSyBBogK5v6Sq4A2vp99iRWyt8j4xQjZfhfA'
-
 stech_url = 'siwiectech.com'
 site_url = 'nlp-aptpdyy7dq-uk.a.run.app'
-#api_key=os.environ['API_KEY']   # this is uploaded as ENV var in the cloud run instance
+api_key=os.environ['API_KEY']   # this is uploaded as ENV var in the cloud run instance
 upload_folder=r''
