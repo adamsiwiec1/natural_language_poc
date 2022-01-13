18f179d91cac24f8f8b34a15387d9cbff43a726d
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-01-13 04:58:20 -0500

    changed to gcp cloud mysql from sqlite that was used for development

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ import os
 
 stech_url = 'siwiectech.com'
 site_url = 'nlp-aptpdyy7dq-uk.a.run.app'
-api_key = 'AIzaSyBBogK5v6Sq4A2vp99iRWyt8j4xQjZfhfA'
-# api_key=os.environ['API_KEY']
+api_key=os.environ['API_KEY']
 upload_folder=r''
